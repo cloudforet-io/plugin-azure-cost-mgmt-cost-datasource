da513e0daa02f0eef2d498fc47c9f7877bab806f
ImMin5 <mino@megazone.com>
2023-06-13 16:07:19 +0900

    feat: modify cost_conf.py

--- src/cloudforet/cost_analysis/conf/cost_conf.py
+++ src/cloudforet/cost_analysis/conf/cost_conf.py
@@ -1,10 +1,11 @@
+RETRY_COUNT = 3
 TYPE = 'ActualCost'
 TIMEFRAME = 'Custom'
 GRANULARITY = 'Daily'
-AGGREGATION = {
-    'totalCost': {'name': 'Cost', 'function': 'Sum'},
-    'UsageQuantity': {'name': 'UsageQuantity', 'function': 'Sum'}
-}
+
+AGGREGATION_USD_COST = {"totalCostUSD": {"name": "CostUSD", "function": "Sum"}}
+AGGREGATION_COST = {"totalCost": {"name": "Cost", "function": "Sum"}}
+AGGREGATION_USAGE_QUANTITY = {"UsageQuantity": {"name": "UsageQuantity", "function": "Sum"}}
 
 GROUPING = [
     {'type': 'Dimension', 'name': 'ResourceGroup'},
@@ -18,43 +19,7 @@ GROUPING = [
     {'type': 'Dimension', 'name': 'UnitOfMeasure'},
 ]
 
-PARAMETERS = {
-    'type': TYPE,
-    'timeframe': TIMEFRAME,
-    'dataset': {
-        'granularity': GRANULARITY,
-        'aggregation': AGGREGATION,
-        'grouping': GROUPING
-    }
-}
-
-AGGREGATION_WITH_USD = {
-    "totalCostUSD": {"name": "CostUSD", "function": "Sum"},
-    'UsageQuantity': {'name': 'UsageQuantity', 'function': 'Sum'}
-}
-
-GROUPING_WITH_TAG = [
-    {'type': 'Dimension', 'name': 'ResourceGroup'},
-    {'type': 'Dimension', 'name': 'ResourceType'},
-    {'type': 'Dimension', 'name': 'ResourceId'},
-    {'type': 'Dimension', 'name': 'ResourceLocation'},
-    {'type': 'Dimension', 'name': 'SubscriptionId'},
-    {'type': 'Dimension', 'name': 'SubscriptionName'},
-    {'type': 'Dimension', 'name': 'MeterCategory'},
-    {'type': 'Dimension', 'name': 'Meter'},
-    {'type': 'Dimension', 'name': 'UnitOfMeasure'},
-    {'type': 'Tag', 'name': ''}
-]
-
-PARAMETERS_WITH_USD_AND_TAG = {
-    'type': TYPE,
-    'timeframe': TIMEFRAME,
-    'dataset': {
-        'granularity': GRANULARITY,
-        'aggregation': AGGREGATION_WITH_USD,
-        'grouping': GROUPING_WITH_TAG
-    }
-}
+GROUPING_TAG_OPTION = {'type': 'Tag', 'name': ''}
 
 REGION_MAP = {
     'global': 'global',
