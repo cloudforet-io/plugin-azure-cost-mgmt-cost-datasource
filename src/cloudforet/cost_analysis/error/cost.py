52a83b3b94f11805bf9b3c75c9c481189e1ae928
ImMin5 <mino@megazone.com>
2023-07-14 16:52:13 +0900

    feat: add error class
    Signed-off-by: ImMin5 <mino@megazone.com>

--- src/cloudforet/cost_analysis/error/cost.py
+++ src/cloudforet/cost_analysis/error/cost.py
@@ -4,10 +4,11 @@ from spaceone.core.error import *
 class ERROR_CONNECTOR_CALL_API(ERROR_UNKNOWN):
     _message = 'API Call Error: {reason}'
 
-
 class ERROR_INVALID_SECRET_TYPE(ERROR_INVALID_ARGUMENT):
     _message = 'Invalid secret type: {secret_type}'
 
+class ERROR_EMPTY_CUSTOMER_TENANTS(ERROR_INVALID_ARGUMENT):
+    _message = 'Empty customer tenants: {customer_tenants}'
 
 class ERROR_INVALID_TOKEN(ERROR_INVALID_ARGUMENT):
     _message = 'Invalid token: {token}'
