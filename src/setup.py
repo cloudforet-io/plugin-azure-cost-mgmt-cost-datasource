c2779d29ea4a834efd2b9a00be175d19243186e1
ImMin5 <mino@megazone.com>
2023-10-31 20:30:37 +0900

    feat: remove spaceone-core package from pip_requirements.txt
    Signed-off-by: ImMin5 <mino@megazone.com>

--- src/setup.py
+++ src/setup.py
@@ -31,7 +31,6 @@ setup(
     license='Apache License 2.0',
     packages=find_packages(),
     install_requires=[
-        'spaceone-core',
         'spaceone-api',
         'schematics',
         'azure-identity',
