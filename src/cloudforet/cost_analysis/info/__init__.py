d85b44cefd24164b76f23db583cf592c191ba360
ImMin5 <mino@megazone.com>
2024-07-31 02:19:42 +0900

    refactor: update plugin framework version to 2.0
    Signed-off-by: ImMin5 <mino@megazone.com>

--- src/cloudforet/cost_analysis/info/__init__.py
+++
@@ -1,4 +0,0 @@
-from cloudforet.cost_analysis.info.common_info import *
-from cloudforet.cost_analysis.info.data_source_info import *
-from cloudforet.cost_analysis.info.job_info import *
-from cloudforet.cost_analysis.info.cost_info import *
--- src/cloudforet/cost_analysis/info/common_info.py
+++
@@ -1,7 +0,0 @@
-from google.protobuf.empty_pb2 import Empty
-
-__all__ = ['EmptyInfo']
-
-
-def EmptyInfo():
-    return Empty()
